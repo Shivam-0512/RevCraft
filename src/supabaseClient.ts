fe7b131bc37affaf854261d25efa6cac3795cc46
Shivam Kushwaha <139127517+Shivam-0512@users.noreply.github.com>
2025-08-15 00:50:10 +0530

    Delete src/supabaseClient.ts

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://scpjbcjiqummkonbfbnf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjcGpiY2ppcXVtbWtvbmJmYm5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NzI1ODcsImV4cCI6MjA3MDE0ODU4N30.XLF-PJ60gliLUrzB6gLiY-yVMddeBjssyMRRPmmPn2s';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
